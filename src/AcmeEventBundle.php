3f4f6b0e50a41068d205ce45b10b448acd2599bf
christophberger <christophberger@users.noreply.github.com>
2023-09-07 10:54:39 +0200

    Bulk changes

--- src/AcmeEventBundle.php
+++ src/AcmeEventBundle.php
@@ -6,7 +6,24 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 /**
  * EventBundle
  */
-class AcmeEventBundle extends Bundle
-{
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-}
+class AcmeEventBundle extends AbstractBundle
+
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // prepend some config option
+        $builder->prependExtensionConfig('sulu_admin', [
+            'lists' => [
+                        'directories' => [
+                            dirname(__DIR__) . '/config/lists',
+                        ],
+            ],
+            'forms' => [
+                        'directories' => [
+                            dirname(__DIR__) . '/config/forms',
+                        ],
+            ],
+            'resources' => ['events' => ['routes' => ['list' => 'app.get_event_list', 'detail' => 'app.get_event']]],
+        ]);
+    }
